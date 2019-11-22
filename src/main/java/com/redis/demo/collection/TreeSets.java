d10a94f80d969a85e8129c32fcb3cd6ea4fda62c
chenlixin <chenlixin_11@163.com>
2019-11-22 09:50:38 +0800

    集合对象排序

---
+++ src/main/java/com/redis/demo/collection/TreeSets.java
@@ -0,0 +1,41 @@
+package com.redis.demo.collection;
+
+import com.redis.demo.po.Student;
+
+import java.util.*;
+
+/**
+ * 项目名称：demo
+ * 类 名 称：TreeSets
+ * 类 描 述：
+ * 创建时间：2019/11/22 09:00
+ * 创 建 人：chenlixin
+ */
+public class TreeSets {
+
+    public static void main(String[] args) {
+        Set set = new TreeSet();
+        set.add(new Student(19,"sansha"));
+        set.add(new Student(10,"sansha"));
+        set.add(new Student(24,"lirouw"));
+        System.out.println(set);
+
+        List<Student> students = new ArrayList<Student>();
+        students.add(new Student(18, "a"));
+        students.add(new Student(19, "q"));
+        students.add(new Student(20, "w"));
+
+        Collections.sort(students, new Comparator<Student>() {
+            @Override
+            public int compare(Student o1, Student o2) {
+                return o1.getAge() > o2.getAge() ? -1 : (o1.getAge().equals(o2.getAge()) ? 0 : 1);
+            }
+        });
+        for (Student student : students) {
+            System.out.println(student.toString());
+        }
+    }
+}
+
+
+
---
+++ src/main/java/com/redis/demo/po/Student.java
@@ -0,0 +1,47 @@
+package com.redis.demo.po;
+
+/**
+ * 项目名称：demo
+ * 类 名 称：Student
+ * 类 描 述：
+ * 创建时间：2019/11/22 09:41
+ * 创 建 人：chenlixin
+ */
+public class Student implements Comparable<Student>{
+    private Integer age;
+    private String name;
+
+    public Student(Integer age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return this.age-o.getAge();
+    }
+}
